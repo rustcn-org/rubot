c02665add1a89da687451295fee41593fbad6e37
YuKun Liu <41265098+mrxiaozhuox@users.noreply.github.com>
2022-04-18 16:31:34 +0800

    提交修复

--- core/route.ts
+++ core/route.ts
@@ -1,7 +1,6 @@
 import { Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
 
 import { hooks } from "../service/hooks.ts";
-import { drawContributors } from "../service/table.ts";
 
 export const router = new Router();
 
@@ -9,13 +8,13 @@ router.get("/", (ctx) => {
 	ctx.response.redirect("https://rustt.org/");
 });
 
-router.get("/table/:type", async (ctx) => {
-	if (ctx.params.type == "contributors") {
-		ctx.response.type = "image/svg+xml";
-		ctx.response.body = await drawContributors();
-	} else {
-		ctx.response.status = 404;
-	}
-});
+// router.get("/table/:type", async (ctx) => {
+// 	if (ctx.params.type == "contributors") {
+// 		ctx.response.type = "image/svg+xml";
+// 		ctx.response.body = await drawContributors();
+// 	} else {
+// 		ctx.response.status = 404;
+// 	}
+// });
 
 router.post("/callback", hooks);
